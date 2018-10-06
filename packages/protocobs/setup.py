76bda0f62f98c3b9d10d97a2d773e6dbb238d4d2
Michael Welter <michael@welter-4d.de>
2018-10-06 20:44:44 +0200

    Turn protocobs into a module - it shall not be a package - because only it's only one file.

--- packages/protocobs/setup.py
+++ packages/protocobs/setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
 
 setup(name='protocobs',
-      version='0.1',
+      version='0.0.1',
       description='COBS framing for protobuf messages',
-      url=None,
-      author='Michael Welter',
-      author_email='michael@welter-4d.de',
-      license='MIT',
-      packages=['protocobs'],
-      zip_safe=False)
+      py_modules=['protocobs'])
