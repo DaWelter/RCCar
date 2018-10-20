2835ff3e0853ac90f40b420b1e556199ae872fc9
Michael Welter <mw.pub@welter-4d.de>
2018-10-20 15:37:37 +0200

    Add option for retransmission

--- app/rccar/scripts/rccar_video_tx.bash
+++ app/rccar/scripts/rccar_video_tx.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /etc/rccar.conf
 
-raspivid --nopreview --awb auto -ih -t 0 -w $WIDTH -h $HEIGHT -fps $FPS -b $BITRATE -n -g $KEYFRAMEINTERVAL -pf high -o - | tx -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $CAR_WIFI
+raspivid --nopreview --awb auto -ih -t 0 -w $WIDTH -h $HEIGHT -fps $FPS -b $BITRATE -n -g $KEYFRAMEINTERVAL -pf high -o - | tx -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH -x $TRANSMISSION_COUNT $CAR_WIFI
